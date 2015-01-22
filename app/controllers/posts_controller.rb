a131825c17102bfdc920de4f4e665f6a9fbbb6bd
William Reilly <William.Reilly2010@gmail.com>
2015-01-21 20:15:05 -0500

    Some index and show controller and views for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,5 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    render :show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  # resources :posts, except: [:destroy] do
+  #   resources :comments, only: [:create, :show]
+  # end
   resources :posts, except: [:destroy] do
-    resources :comments, only: [:create, :show]
+    resources :comments, except: [:destroy]
   end
-
 end
